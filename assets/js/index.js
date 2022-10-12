0f392f15fc49fea231916c76c4cd570015c2cc8f
murraydeam <73445952+murraydeam@users.noreply.github.com>
2022-10-12 12:28:19 -0400

    Pen hover state logic has been added. A button to allow the user to set the size of the grid is the next feature to be added.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
 let columns = 16;
-let rows = 16;
+let gridItems = 16;
+
+const resetButton = document.createElement('button')
+resetButton.setAttribute('id', 'resetButton')
+
+
+
 
 let grid = document.createElement("div");
 grid.setAttribute("id", "grid");
@@ -7,11 +13,17 @@ grid.setAttribute("id", "grid");
 for (let i = 0; i < columns; ++i) {
   let column = document.createElement("div");
   column.className = "column";
-  for (let j = 0; j < rows; ++j) {
-    let row = document.createElement("div");
-    row.className = "row";
-    column.appendChild(row);
+  for (let j = 0; j < gridItems; ++j) {
+    let gridItem = document.createElement("div");
+    gridItem.className = "gridItem";
+    gridItem.addEventListener("mouseover", mouseOver);
+    function mouseOver() {
+      gridItem.style.backgroundColor = "red";
+    }
+
+    column.appendChild(gridItem);
   }
+
   grid.appendChild(column);
 }
 
